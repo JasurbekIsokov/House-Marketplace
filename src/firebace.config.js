2e2f1cf1b4a8b993384bd66227f37f93bfb7074b
Jasurbek <jasurisoqov685@gmail.com>
2022-07-15 21:08:22 +0500

    Folder and File structure and create firebase config

---
+++ src/firebace.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebace/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDeWCcJpJy6Gd0nCx36RY5LpbYYCzvPGlM",
+  authDomain: "house-marketplace-app-ji.firebaseapp.com",
+  projectId: "house-marketplace-app-ji",
+  storageBucket: "house-marketplace-app-ji.appspot.com",
+  messagingSenderId: "232120746751",
+  appId: "1:232120746751:web:ceea2963388467248c82ca",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
