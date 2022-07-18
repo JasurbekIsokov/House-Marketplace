f9736c99e8c8f1e3ec61f9faeb13998aaff42c0a
Jasurbek <jasurisoqov685@gmail.com>
2022-07-18 19:24:25 +0500

    signIn email and password input and forgotPasword

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,59 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+
 const SignIn = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+
+  const navigate = useNavigate();
+
+  const onChange = () => {};
+
   return (
-    <div>
-      <h1>SignIn</h1>
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Welcome Back!</p>
+      </header>
+
+      <form>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="emailInput"
+          placeholder="email"
+          value={email}
+          onChange={onChange}
+        />
+
+        <div className="passwordInputDiv">
+          <input
+            type={showPassword ? "text" : "password"}
+            className="passwordInput"
+            id="password"
+            placeholder="Password"
+            value={password}
+            onChange={onChange}
+          />
+
+          <img
+            src={visibilityIcon}
+            alt="show password"
+            className="showPassword"
+            onClick={() => setShowPassword((prevState) => !prevState)}
+          />
+        </div>
+
+        <Link className="forgotPasswordLink" to="/forgot-password">
+          Forgot Password
+        </Link>
+      </form>
     </div>
   );
 };
