31c6bda5fdc124bf62a8ce84decbfad04ecb0f27
Jasurbek <jasurisoqov685@gmail.com>
2022-07-19 16:20:20 +0500

    socialLogin

--- src/Components/OAuth.jsx
+++ src/Components/OAuth.jsx
@@ -6,7 +6,19 @@ import { toast } from "react-toastify";
 import googleIcon from "../assets/svg/googleIcon.svg";
 
 const OAuth = () => {
-  return <div>OAuth</div>;
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const onGoogleClick = () => {};
+
+  return (
+    <div className="socialLogin">
+      <p>Sign {location.pathname === "/sign-up" ? "up" : "in"} with</p>
+      <button className="socialIconDiv" onClick={onGoogleClick}>
+        <img className="socialIconImg" src={googleIcon} alt="google" />
+      </button>
+    </div>
+  );
 };
 
 export default OAuth;
