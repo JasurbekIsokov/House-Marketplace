3fd9bdf13194fd7c32c813821f04e0adc656a4e1
Jasurbek <jasurisoqov685@gmail.com>
2022-07-19 14:43:40 +0500

    useAuthStatus

---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+export const useAuthStatus = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checkingStatus, setChackingStatus] = useState(true);
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggedIn(true);
+      }
+      setChackingStatus(false);
+    });
+  });
+
+  return { loggedIn, checkingStatus };
+};
