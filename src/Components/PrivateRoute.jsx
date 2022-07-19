0a5ca84bdcc71fb8ff030ef88faefc88bcc45ce4
Jasurbek <jasurisoqov685@gmail.com>
2022-07-19 14:52:45 +0500

    costume hooks useAuthStatus

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -1,9 +1,14 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStatus } from "../hooks/useAuthStatus";
 
 const PrivateRoute = () => {
-  const loggedId = false;
+  const { loggedIn, checkingStatus } = useAuthStatus();
 
-  return loggedId ? <Outlet /> : <Navigate to="/sign-in" />;
+  if (checkingStatus) {
+    return <h3>Loading...</h3>;
+  }
+
+  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
 };
 
 export default PrivateRoute;
--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,19 +1,26 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export const useAuthStatus = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [checkingStatus, setChackingStatus] = useState(true);
+  const isMounted = useRef(true);
 
   useEffect(() => {
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setLoggedIn(true);
-      }
-      setChackingStatus(false);
-    });
-  });
+    if (isMounted) {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true);
+        }
+        setChackingStatus(false);
+      });
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
 
   return { loggedIn, checkingStatus };
 };
