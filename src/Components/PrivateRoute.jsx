2464020b2c1a15dae38ff74a30abb96a84eff4d3
Jasurbek <jasurisoqov685@gmail.com>
2022-07-19 14:55:41 +0500

    Spiinner

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -1,11 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuthStatus } from "../hooks/useAuthStatus";
+import Spinner from "./Spinner";
 
 const PrivateRoute = () => {
   const { loggedIn, checkingStatus } = useAuthStatus();
 
   if (checkingStatus) {
-    return <h3>Loading...</h3>;
+    return <Spinner />;
   }
 
   return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="loadingSpinnerContainer">
+      <div className="loadigSpinner"></div>
+    </div>
+  );
+};
+
+export default Spinner;
