6aef6ae6a5b22243d6931c19ed11b70e790bf437
Jasurbek <jasurisoqov685@gmail.com>
2022-07-19 15:55:40 +0500

    import elemnts in OAuth

--- src/Components/OAuth.jsx
+++ src/Components/OAuth.jsx
@@ -1,3 +1,10 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { doc, setDoc, getDoc } from "firebase/firestore";
+import { db } from "../firebace.config";
+import { toast } from "react-toastify";
+import googleIcon from "../assets/svg/googleIcon.svg";
+
 const OAuth = () => {
   return <div>OAuth</div>;
 };
