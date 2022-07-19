51488c2f19e3ab8d94ffd765a54d05e8b54bdc47
Jasurbek <jasurisoqov685@gmail.com>
2022-07-19 10:55:44 +0500

    profile Formdata

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+import { getAuth } from "firebase/auth";
+import { useNavigate, Link } from "react-router-dom";
+
 const Profile = () => {
+  const auth = getAuth();
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const { name, email } = formData;
+
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button className="logOut" type="button" onClick={onLogout}>
+          Logout
+        </button>
+      </header>
     </div>
   );
 };
