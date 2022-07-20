1ae00cb7774032ddea3eb44a3a72d17c3c202021
Jasurbek <jasurisoqov685@gmail.com>
2022-07-20 05:28:44 +0500

    onGoogleClick

--- src/Components/OAuth.jsx
+++ src/Components/OAuth.jsx
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { doc, setDoc, getDoc } from "firebase/firestore";
+import { doc, setDoc, getDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebace.config";
 import { toast } from "react-toastify";
 import googleIcon from "../assets/svg/googleIcon.svg";
@@ -9,7 +9,30 @@ const OAuth = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const onGoogleClick = () => {};
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // Check for user
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      // If user, doesn't exist, create user
+      if (!docSnap.exists()) {
+        await setDoc(doc(db, "users", user.uid), {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+    }
+  };
 
   return (
     <div className="socialLogin">
