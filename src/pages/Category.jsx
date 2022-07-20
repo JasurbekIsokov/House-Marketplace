9e5838183f50e9df759bdd772f4f5de8584f0ca6
Jasurbek <jasurisoqov685@gmail.com>
2022-07-20 11:44:52 +0500

    import element category

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,3 +1,18 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  getDoc,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import { db } from "../firebace.config";
+import { toast } from "react-toastify";
+import Spinner from "../Components/Spinner";
+
 const Category = () => {
   return (
     <div>
