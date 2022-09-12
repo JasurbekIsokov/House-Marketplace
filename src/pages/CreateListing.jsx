42a631468545e113938d4173189b0500616c43a4
Jasurbek <jasurisoqov685@gmail.com>
2022-09-12 08:08:32 +0500

    Create lisitng page location fixed

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -176,9 +176,9 @@ const CreateListing = () => {
       timestamp: serverTimestamp(),
     };
 
+    // formDataCopy.location = address;
     delete formDataCopy.images;
     delete formDataCopy.address;
-    // location && (formDataCopy.location = location)
     !formDataCopy.offer && delete formDataCopy.discountedPrice;
 
     const docRef = await addDoc(collection(db, "listings"), formDataCopy);
