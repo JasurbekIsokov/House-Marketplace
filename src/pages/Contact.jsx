3b9d8131a4daf38d2a067a73d0eedd38e22cae8e
Jasurbek <jasurisoqov685@gmail.com>
2022-09-13 08:38:26 +0500

    Contact page getLandlord function

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,33 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebace.config";
+import { toast } from "react-toastify";
 
 const Contact = () => {
+  const [message, setMessage] = useState("");
+  const [landlord, setLandlord] = useState(null);
+  // eslint-disable-next-line
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const params = useParams();
+
+  useEffect(() => {
+    const getLandlord = async () => {
+      const docRef = doc(db, "users", params.landlordId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setLandlord(docSnap.data());
+      } else {
+        toast.error("Could not get landlord data");
+      }
+    };
+
+    getLandlord();
+  }, [params.landlordId]);
+
   return <div>Contact</div>;
 };
 
