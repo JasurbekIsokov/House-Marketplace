3b85435f08e761409cc3812d8efc2a4a9768039f
Jasurbek <jasurisoqov685@gmail.com>
2022-09-13 08:24:12 +0500

    Listing page fetchListing

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -7,6 +7,29 @@ import Spinner from "../Components/Spinner";
 import shareIcon from "../assets/svg/shareIcon.svg";
 
 const Listing = () => {
+
+   const [listing, setListing] = useState(null);
+   const [loading, setLoading] = useState(true);
+   const [shareLinkCopied, setShareLinkCopied] = useState(false);
+
+   const navigate = useNavigate();
+   const params = useParams();
+   const auth = getAuth();
+
+    useEffect(() => {
+      const fetchListing = async () => {
+        const docRef = doc(db, "listings", params.listingId);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          setListing(docSnap.data());
+          setLoading(false);
+        }
+      };
+
+      fetchListing();
+    }, [navigate, params.listingId]);
+
   return <div>LISTING</div>;
 };
 
