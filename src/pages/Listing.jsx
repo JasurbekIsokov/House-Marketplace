c97f54934cf687f62df7ded0a0117982bef43ff7
Jasurbek <jasurisoqov685@gmail.com>
2022-09-18 12:31:28 +0500

    listing page map

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -3,6 +3,8 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 
 import { Helmet } from "react-helmet";
 
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
 import { getDoc, doc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { db } from "../firebace.config";
@@ -121,6 +123,26 @@ const Listing = () => {
         </ul>
 
         <p className="listingLocationTitle">Location</p>
+
+        <div className="leafletContainer">
+          <MapContainer
+            style={{ height: "100%", width: "100%" }}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
+            />
+
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>{listing.location}</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
 
         {auth.currentUser?.uid !== listing.userRef && (
           <Link
